791caec324a392e8c12817b462a9c39da4d65b97
Alexey Bolotov <stealthfear@gmail.com>
2021-10-11 12:12:36 +0500

    Add function print status and messages count when start settings.py

--- settings.py
+++ settings.py
@@ -8,9 +8,11 @@ smtp_port = ""  #
 if __name__ == "__main__":
     '''Test connect to imap server, get status connect, get numbers of messages'''
     from imaplib import IMAP4_SSL
+    import re
 
     imap = IMAP4_SSL(imap)
     imap.login(username, password)
     status, messages = imap.select("INBOX")
     print(f'Staus connect: {status}')
-    print(f'INBOX messages: {messages}')
+    messages_count = re.findall("\d+", str(messages))
+    print(f"INBOX messages: {messages_count[0]}")
